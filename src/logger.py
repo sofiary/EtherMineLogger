406e7cbbcbc742bec962906a6c5095b78f0041dc
sofiary <33208002+sofiary@users.noreply.github.com>
2021-08-08 21:52:59 +0800

    incremental changes

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,10 @@
 import os
 import psycopg2
 
-API_ENDPOINT = r"https://api.ethermine.org/"
+API_ENDPOINT: str = r"https://api.ethermine.org/"
+MINER: str = os.environ.get('EML_MINER')
+WORKERS: list = os.environ.get('EML_WORKERS').split(',')
 
+if __name__=="__main__":
+    pass
 
