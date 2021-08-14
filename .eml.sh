04240723d8ba003918bffc8e28fcdde836127873
sofiary <33208002+sofiary@users.noreply.github.com>
2021-08-15 03:10:01 +0800

    Added a postgreSQL port check which is echoed to the terminal

--- .eml.sh
+++ .eml.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/sh
 
-# Helper script
+# Execution script
 
 source ./venv/bin/activate
 sudo service postgresql start
+# The next command exposes which port PostgreSQL is running on
+pgport=sudo sed -n 4p /var/lib/postgresql/12/main/postmaster.pid
+echo PostgreSQL server running on port $pgport
 python3 ./src/logger.py
